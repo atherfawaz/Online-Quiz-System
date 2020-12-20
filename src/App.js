802303bed8a3ac1eb0e8acfe3e61bfed8a737e22
khizar hussain <khezarhussain1999@gmail.com>
2020-12-20 19:25:26 +0500

    middle of account settings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import LoginRegister from "./pages/loginregister";
 import Home from "./pages/home";
 import InvalidPage from "./pages/invalidPage";
+import AccountSettings from "./pages/accountSettings"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact component={Home}></Route>
         <Route path="/home" exact component={Home}></Route>
         <Route path="/login" exact component={LoginRegister}></Route>
+        <Route path="/account-settings" exact component={AccountSettings}></Route>
         <Route component={InvalidPage}></Route>
       </div>
     </Router>

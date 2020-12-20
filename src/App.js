baf81922660ee5ec19c9a218ff8fee9189636613
khizar hussain <khezarhussain1999@gmail.com>
2020-12-21 00:18:50 +0500

    final push

--- src/App.js
+++ src/App.js
@@ -12,11 +12,8 @@ import QuizResult from "./pages/quizResult";
 import Quiz from "./pages/quiz";
 import CreateClass from "./pages/createclass";
 import createQuiz from "./pages/createQuiz";
-<<<<<<< HEAD
 import AddQuestions from "./pages/addQuestions";
-=======
 import ViewQuizHistory from "./pages/viewquizhistory";
->>>>>>> 0b25117b657b64f11be0d774d0374c03874638a7
 
 function App() {
   return (
@@ -36,11 +33,8 @@ function App() {
         <Route path="/quizresult" exact component={QuizResult}></Route>
         <Route path="/createclass" exact component={CreateClass}></Route>
         <Route path="/createquiz" exact component={createQuiz}></Route>
-<<<<<<< HEAD
         <Route path="/addquestions" exact component={AddQuestions}></Route>
-=======
         <Route path="/viewquizhistory" exact component={ViewQuizHistory}></Route>
->>>>>>> 0b25117b657b64f11be0d774d0374c03874638a7
       </div>
     </Router>
   );

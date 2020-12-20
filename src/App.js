46ce0bd4afb2e036c8474680f17d06cca932b567
atherfawaz <atherfawaz@users.noreply.github.com>
2020-12-20 18:47:06 +0500

    added coursegrid router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import "./components/NavBar";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import LoginRegister from "./pages/loginregister";
 import Home from "./pages/home";
+import CourseGrid from "./pages/CourseGrid";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" exact component={Home}></Route>
         <Route path="/home" exact component={Home}></Route>
         <Route path="/login" exact component={LoginRegister}></Route>
+        <Route path="/courses" exact component={CourseGrid}></Route>
       </div>
     </Router>
   );
--- src/pages/loginregister.js
+++ src/pages/loginregister.js
@@ -5,7 +5,7 @@ import NavBar from "../components/NavBar";
 
 function LoginRegister() {
   return (
-    <div class="wapper">
+    <div className="wapper">
       <Loader />
       <NavBar />
       <div>

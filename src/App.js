dab1acb3bbafdb5c184de2e2dd2d5b4a2cf5d2d9
SovereignGhost <xenither@gmail.com>
2020-12-20 20:29:39 +0500

    accept app from latest pull

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import LoginRegister from "./pages/loginregister";
 import Home from "./pages/home";
 import InvalidPage from "./pages/invalidPage";
-<<<<<<< HEAD
-import AccountSettings from "./pages/accountSettings"
-=======
 import AccountSettings from "./pages/accountSettings";
 import CourseGrid from "./pages/CourseGrid";
->>>>>>> 67389dc4387d5c1a6708c60a60b87ed53ba40766
 
 function App() {
   return (
@@ -18,17 +14,12 @@ function App() {
         <Route path="/" exact component={Home}></Route>
         <Route path="/home" exact component={Home}></Route>
         <Route path="/login" exact component={LoginRegister}></Route>
-<<<<<<< HEAD
-        <Route path="/account-settings" exact component={AccountSettings}></Route>
-        <Route component={InvalidPage}></Route>
-=======
         <Route path="/courses" exact component={CourseGrid}></Route>
         <Route
           path="/account-settings"
           exact
           component={AccountSettings}
         ></Route>
->>>>>>> 67389dc4387d5c1a6708c60a60b87ed53ba40766
       </div>
     </Router>
   );

c0917565c5bd2eb0a25f21744b96bae3dc8d7e40
khizar hussain <khezarhussain1999@gmail.com>
2021-01-06 17:48:36 +0500

    fixed p env code

--- Server/server.js
+++ Server/server.js
@@ -48,8 +48,8 @@ mongoose.connect(
 
 if(process.env.NODE_ENV === 'production'){
   const path  =  require('path');
-  app.get('/*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(publicPath,'index.html'))
   })
 }
 else{
